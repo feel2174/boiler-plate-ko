93ed3d4db5b346a6029c0e48c2b4cdf71dcacb54
feel2174 <feel2174@gmail.com>
2020-07-12 14:36:58 +0900

    Added NodeMon

---
+++ config/prod.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI: 'mongodb+srv://devzucca:fkdlzm10@boilerplate.piuvp.mongodb.net/BoilerPlate?retryWrites=true&w=majority'
+}
--- index.js
+++ index.js
@@ -1,12 +1,42 @@
 const express = require('express')
 const app = express()
 const port = 5000
+const bodyParser = require('body-parser');
+const { User } = require('./models/User');
+const config = require('./config/key')
+
+//application/x-www-from-urlencoded
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(bodyParser.json());
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://devzucca:fkdlzm10@boilerplate.piuvp.mongodb.net/BoilerPlate?retryWrites=true&w=majority', {
+mongoose.connect(config.mongoURI, {
     useNewUrlParser: true, useUnifiedTopology:true, useCreateIndex: true, useFindAndModify: false}).then(() => console.log('MongoDB connected...'))
 .catch(err => console.log(err))
 
-app.get('/', (req, res) => res.send('Hello World! ~ 안녕하세요 devZucca'))
+app.get('/', (req, res) => res.send('Hello World!'))
+
+
+app.post('/register', (req, res) => {
+    //회원가입 할때 필요한 정보들을 client에서 가져오면
+    //그것들을 DB엔 넣어준다.
+
+    const user = new User(req.body)
+
+    user.save((err, userInfo) => {
+        if(err) return res.json({ suceess: false, err})
+        return res.status(200).json({
+            success: true
+        })
+    })
+
+
+})
+
+
+
+
+
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
