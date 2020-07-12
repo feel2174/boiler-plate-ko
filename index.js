d5585d6a3d66bf85d4755f0ff4d595a2550b3c43
feel2174 <feel2174@gmail.com>
2020-07-12 13:48:15 +0900

    처음 저장소에 올림

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express()
+const port = 5000
+
+const mongoose = require('mongoose')
+mongoose.connect('mongodb+srv://devzucca:fkdlzm10@boilerplate.piuvp.mongodb.net/BoilerPlate?retryWrites=true&w=majority', {
+    useNewUrlParser: true, useUnifiedTopology:true, useCreateIndex: true, useFindAndModify: false}).then(() => console.log('MongoDB connected...'))
+.catch(err => console.log(err))
+
+app.get('/', (req, res) => res.send('Hello World! ~ 안녕하세요 devZucca'))
+
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
